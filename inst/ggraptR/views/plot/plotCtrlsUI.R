311489108e1f1e51620de8832ed063d7cb0530d4
Gray <grayskripko@gmail.com>
2016-08-31 17:39:00 +0200

    removed redundant ui controls for ggpairs

--- inst/ggraptR/views/plot/plotCtrlsUI.R
+++ inst/ggraptR/views/plot/plotCtrlsUI.R
@@ -14,25 +14,27 @@ verticalLayout(
     source('./views/plot/aesCtrlsUI.R', local=TRUE)$value,
     class="widblock"),
   
-  div(
-    uiOutput('showFacetWgtsCtrl'),
-    source('./views/plot/facetCtrlsUI.R', local=TRUE)$value,
-    class="widblock"),
-  
-  div(
-    uiOutput('showXYRangeWgtsCtrl'),
-    source('./views/plot/xyRangeCtrlsUI.R', local=TRUE)$value,
-    class="widblock"),
-  
-  div(
-    uiOutput('showThemeWgtsCtrl'),
-    source('./views/plot/labelAndStyleCtrlsUI.R', local=TRUE)$value,
-    class="widblock"),
-  
-  div(
-    uiOutput('showDSTypeAndPlotAggWgtsCtrl'),
-    source('./views/plot/DSTypeAndPlotAggCtrlsUI.R', local=TRUE)$value,
-    class="widblock")
-  
-  #uiOutput('showPlotAggWgtCtrl')
+  conditionalPanel('input.plotType != "pairs"',
+    div(
+      uiOutput('showFacetWgtsCtrl'),
+      source('./views/plot/facetCtrlsUI.R', local=TRUE)$value,
+      class="widblock"),
+    
+    div(
+      uiOutput('showXYRangeWgtsCtrl'),
+      source('./views/plot/xyRangeCtrlsUI.R', local=TRUE)$value,
+      class="widblock"),
+    
+    div(
+      uiOutput('showThemeWgtsCtrl'),
+      source('./views/plot/labelAndStyleCtrlsUI.R', local=TRUE)$value,
+      class="widblock"),
+    
+    div(
+      uiOutput('showDSTypeAndPlotAggWgtsCtrl'),
+      source('./views/plot/DSTypeAndPlotAggCtrlsUI.R', local=TRUE)$value,
+      class="widblock")
+    
+    #uiOutput('showPlotAggWgtCtrl')
+  )
 )
