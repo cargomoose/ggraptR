2b486db71f03f0a64483e95fc4f1892ccbf3af51
Gray <grayskripko@gmail.com>
2016-10-30 11:38:09 +0200

    minor refactoring WidgetsNames.R

--- inst/ggraptR/reactives/plotWidgetNames.R
+++ inst/ggraptR/reactives/plotWidgetNames.R
@@ -1,84 +1,72 @@
-facetWidgets <- c('facetRow', 'facetCol', 'facetWrap', 'facetScale')
+facetWidgets <- reactive({ 
+  if (showFacetWgts()) c('facetRow', 'facetCol', 'facetWrap', 'facetScale')
+})
+XYRangeWidgets <- reactive({ 
+  if (showXYRangeWgts()) c('xlim', 'ylim')
+})
+
 
 scatterWidgets <- reactive({
   flog.debug("plotWidgetNames::scatterWidgets()", name='all')
-  if (is.null(input$showAesWgts)) return()
-  c('plotType', 'x', 'y',
-    if (input$showAesWgts) c('treatAsFacVarCol', 'shape', 'size', 'smooth', 
-                             'jitter' , 'sizeMag', 'color'),
-    if (showXYRangeWgts()) c('xlim', 'ylim'),
-    if (input$showFacetWgts) facetWidgets)
+  if (is.null(showAesWgts())) return()
+  c('plotType', 'x', 'y', XYRangeWidgets(), facetWidgets(),
+    if (showAesWgts()) c('treatAsFacVarCol', 'shape', 'size', 'smooth', 
+                         'jitter' , 'sizeMag', 'color'))
 })
 
 lineWidgets <- reactive({
   flog.debug("plotWidgetNames::lineWidgets() - Start", name='all')
-  c('plotType', 'x', 'y',
-    if (input$showAesWgts) 'color',
-    if (showXYRangeWgts()) c('xlim', 'ylim'),
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', 'y', XYRangeWidgets(), facetWidgets(),
+    if (showAesWgts()) 'color')
 })
 
 ## lineplot points
 linePtsOverlayWidgets <- reactive({
   flog.debug("plotWidgetNames::linePtsOverlayWidgets()", name='all')
   c('pointsOverlay', 
-    if (input$showAesWgts) c('x', 'shape', 'size', 'smooth', 'jitter', 'sizeMag'))
+    if (showAesWgts()) c('x', 'shape', 'size', 'smooth', 'jitter', 'sizeMag'))
 })
 
 pathWidgets <- reactive({
   flog.debug("plotWidgetNames::pathWidgets()", name='all')
-  c('plotType', 'x', 'y',
-    if (showXYRangeWgts()) c('xlim', 'ylim'),
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', 'y', XYRangeWidgets(), facetWidgets())
 })
 
 pathPtsOverlayWidgets <- reactive({
   flog.debug("plotWidgetNames::pathPtsOverlayWidgets()", name='all')
-  c('pointsOverlay',
-    if (showAesWgts()) c('shape', 'size'))
+  c('pointsOverlay', if (showAesWgts()) c('shape', 'size'))
 })
 
 histogramWidgets <- reactive({
   flog.debug("plotWidgetNames::histogramWidgets()", name='all')
-  c('plotType', 'x', 
-    if (input$showAesWgts) c('fill', 'position', 'binWidth'),
-    if (showXYRangeWgts()) 'xlim',
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', facetWidgets(), if (showXYRangeWgts()) 'xlim'
+    if (showAesWgts()) c('fill', 'position', 'binWidth'))
 })
 
 densityWidgets <- reactive({
   flog.debug("plotWidgetNames::densityWidgets()", name='all')
-  c('plotType', 'x', 
-    if (showXYRangeWgts()) 'xlim',
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', facetWidgets(), if (showXYRangeWgts()) 'xlim')
 })
 
 boxWidgets <- reactive({
   flog.debug("plotWidgetNames::boxWidgets()", name='all')
-  c('plotType', 'x', 'y',
-    if (input$showAesWgts) 'fill',
-    if (showXYRangeWgts()) c('xlim', 'ylim'),
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', 'y', XYRangeWidgets(), facetWidgets(), if (showAesWgts()) 'fill')
 })
 
 barWidgets <- reactive({
   flog.debug("plotWidgetNames::barWidgets()", name='all')
-  c('plotType', 'x', 'y',
-    if (input$showAesWgts) c('fill', 'position'),
-    if (showXYRangeWgts()) c('xlim', 'ylim'),
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', 'y', XYRangeWidgets(), facetWidgets(), 
+    if (showAesWgts()) c('fill', 'position'))
 })
 
 violinWidgets <- reactive({
   flog.debug("plotWidgetNames::violinWidgets()", name='all')
-  c('plotType', 'x', 'y',
-    if (input$showAesWgts) c('fill'),
-    if (input$showFacetWgts) facetWidgets)
+  c('plotType', 'x', 'y', facetWidgets(), if (showAesWgts()) c('fill'))
 })
 
 pairsWidgets <- reactive({
   flog.debug("plotWidgetNames::pairshWidgets()", name='all')
-  c('plotType', 'columns', if (input$showAesWgts) c('fill', 'color'))
+  c('plotType', 'columns', if (showAesWgts()) c('fill', 'color'))
 })
 
 themeWidgets <- reactive({
