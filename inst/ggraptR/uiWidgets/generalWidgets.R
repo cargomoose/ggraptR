76880db262dda552f8b1473eb379b54263463f38
Gray <grayskripko@gmail.com>
2017-02-19 02:06:26 +0200

    codeTab. Assignment of new dataset. Fix

--- inst/ggraptR/uiWidgets/generalWidgets.R
+++ inst/ggraptR/uiWidgets/generalWidgets.R
@@ -24,5 +24,19 @@ output$evalConsoleBtn <- renderUI({
 
 output$consoleCtrl <- renderText({
   input$evalConsoleBtn  # dependency
-  isolate(eval(parse(text = input$console)))
+  
+  isolate({
+      console_input <- gsub('(?<=[^<])<-', '<<-', input$console, perl = T)
+      res <- isolate(eval(parse(text = console_input)))
+      tryCatch({
+        cat(res, fill = T)  # check renderText will be able to process it with inner cat()
+        res
+    }, error = function(e) {
+      if (grepl("cannot be handled by 'cat'", e$message)) {
+        if (is.list(res)) capture.output(str(res)) else '[Success]'
+      } else {
+        stop(e$message)
+      }
+    })
+  })
 })
