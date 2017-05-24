5808a0ad6c5a9ab2f3c3e65f439e234fafd64ae5
gray <grayskripko@gmail.com>
2017-05-24 18:42:18 +0200

    added informative error message to prevent assigning non-global variables on console tab

--- inst/ggraptR/uiWidgets/generalWidgets.R
+++ inst/ggraptR/uiWidgets/generalWidgets.R
@@ -25,12 +25,22 @@ output$consoleCtrl <- renderText({
   input$evalConsoleBtn  # dependancy
   
   isolate({
-      console_input <- gsub('(?<=[^<])<-', '<<-', input$console, perl = T)
+    console_input <- gsub('(?<=[^<])<-', '<<-', input$console, perl = T)
+    
+    tryCatch({
       res <- capture.output(eval(parse(text = console_input)))
-      paste(tryCatch({
-        cat(res, fill = T)  # check renderText will be able to process it with inner cat()
-        res
     }, error = function(e) {
+      if (grepl('cannot change value of locked binding for', e$message)) {
+        e$message <- "Can change value for .GlobalEnv variables only"
+      }
+      stop(e$message)
+    })
+    
+    paste(tryCatch({
+      cat(res, fill = T)  # check renderText will be able to process it with inner cat()
+      res
+    }, error = function(e) {
+      browser()
       if (grepl("cannot be handled by 'cat'", e$message)) {
         if (is.list(res)) str(res) else '[Success]'
       } else {
