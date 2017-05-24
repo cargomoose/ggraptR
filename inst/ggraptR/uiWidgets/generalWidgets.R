f0f87dddb41e23ecca2bf2817daeb5756872d9c2
gray <grayskripko@gmail.com>
2017-05-24 18:12:05 +0200

    added <pre> to console output. Fixed some console.output problems

--- inst/ggraptR/uiWidgets/generalWidgets.R
+++ inst/ggraptR/uiWidgets/generalWidgets.R
@@ -22,20 +22,20 @@ output$evalConsoleBtn <- renderUI({
 })
 
 output$consoleCtrl <- renderText({
-  input$evalConsoleBtn  # dependency
+  input$evalConsoleBtn  # dependancy
   
   isolate({
       console_input <- gsub('(?<=[^<])<-', '<<-', input$console, perl = T)
-      res <- isolate(eval(parse(text = console_input)))
-      tryCatch({
+      res <- capture.output(eval(parse(text = console_input)))
+      paste(tryCatch({
         cat(res, fill = T)  # check renderText will be able to process it with inner cat()
         res
     }, error = function(e) {
       if (grepl("cannot be handled by 'cat'", e$message)) {
-        if (is.list(res)) capture.output(str(res)) else '[Success]'
+        if (is.list(res)) str(res) else '[Success]'
       } else {
         stop(e$message)
       }
-    })
+    }), collapse = '\n')
   })
 })
--- inst/ggraptR/views/codeCtrlsUI.R
+++ inst/ggraptR/views/codeCtrlsUI.R
@@ -4,5 +4,5 @@ div(
     column(8, textInput('console', NULL, placeholder = 'Enter R command here')),
     column(3, uiOutput('evalConsoleBtn'))),
   
-  div(textOutput('consoleCtrl'), style="padding-left: 10px; padding-right: 40px;")
+  pre(textOutput('consoleCtrl'), style="padding-left: 10px; padding-right: 40px;")
 )
