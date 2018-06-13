288c7682610f9e3050740617ab3dad69f2d7b1d9
gray <grayskripko@gmail.com>
2018-06-13 13:34:48 +0200

    the reason why autotesting does not work

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,7 @@ library(ggraptR)
 
 
 if (Sys.getenv("NOT_CRAN") == "true") {  # like global skip_on_cran
+  # https://github.com/r-lib/testthat/issues/144#issuecomment-396902791
   Sys.setenv("R_TESTS" = "")  # accroding to https://github.com/hadley/testthat/issues/144
   test_check("ggraptR")
 }
